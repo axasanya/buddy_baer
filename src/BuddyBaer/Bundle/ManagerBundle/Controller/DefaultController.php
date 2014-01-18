3a398e42599977e95c6f492f11bf32ce8d232248
axasanya <sandor.akszenovics@gmail.com>
2014-01-18 19:05:05 +0100

    playing with markers

--- src/BuddyBaer/Bundle/ManagerBundle/Controller/DefaultController.php
+++ src/BuddyBaer/Bundle/ManagerBundle/Controller/DefaultController.php
@@ -4,14 +4,42 @@ namespace BuddyBaer\Bundle\ManagerBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Ivory\GoogleMap\Overlays\Animation;
+use Ivory\GoogleMap\Overlays\Marker;
+
+
+
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        /** @var Ivory\GoogleMapBundle\Model\Map */
+        /** @var Ivory\GoogleMapBundle\Entity\Map */
         $map = $this->get('ivory_google_map.map');
+
+        $markerFromConfig = $this->get('ivory_google_map.marker');
+        $map->addMarker($markerFromConfig);
+
+
+        $marker = new Marker();
+// Configure your marker options
+        $marker->setPrefixJavascriptVariable('marker_');
+        $marker->setPosition( 52.5343700,13.4305300, true);
+        $marker->setAnimation(Animation::DROP);
+
+        $marker->setOption('clickable', false);
+        $marker->setOption('flat', true);
+        $marker->setOptions(array(
+            'clickable' => false,
+            'flat'      => true,
+        ));
+
+        $map->addMarker($marker);
+
        // var_dump("<pre>", $map);die;
 
-        return $this->render('BuddyBaerManagerBundle:Default:index.html.twig', array('map' => $map));
+        return $this->render('BuddyBaerManagerBundle:Default:index.html.twig',
+            array('map' => $map,
+               // 'marker' => $marker
+        ));
     }
 }
